e63ce8d4992fbcd999fc0fdc17e69a4025aa8fb4
crandonbruz <bcruz12127@gmail.com>
2023-08-03 21:22:14 -0400

    added necessary functions

--- index.js
+++ index.js
@@ -13,49 +13,143 @@ const db = mysql.createConnection(
 );
 
 const questions = () => {
-  inquirer.prompt([
-    {
-      type: "list",
-      message: "What would you like to do today?",
-      name: "selectActivity",
-      choices: [
-        "view all departments",
-        "view all roles",
-        "view all employees",
-        "add a department",
-        "add a role",
-        "add an empoyee",
-        "update an employee role",
-      ],
-    },
-  ]);
-  .then((answers) => {
-    const {choices} = answers;
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What would you like to do today?",
+        name: "selectActivity",
+        choices: [
+          "view all departments",
+          "view all roles",
+          "view all employees",
+          "add a department",
+          "add a role",
+          "add an employee",
+          "update an employee role",
+        ],
+      },
+    ])
+    .then((result) => {
+      const { selectActivity } = result;
 
-    if (choices === "view all departments"){
+      if (selectActivity === "view all departments") {
         viewAllDepartments();
-    }
-    if (choices === "view all roles"){
+      }
+      if (selectActivity === "view all roles") {
         viewAllRoles();
-    }
-    if (choices === "view all employees"){
-        viewAllEmloyees();
-    }
-    if (choices === "add a department"){
+      }
+      if (selectActivity === "view all employees") {
+        viewAllEmployees();
+      }
+      if (selectActivity === "add a department") {
         addDepartment();
-    }
-    if (choices === "add a role"){
+      }
+      if (selectActivity === "add a role") {
         addRoles();
-    }
-    if (choices === "add an employee"){
+      }
+      if (selectActivity === "add an employee") {
         addEmployee();
-    }
-    if (choices === "update an employee role"){
+      }
+      if (selectActivity === "update an employee role") {
         updateEmployeeRole();
-    }
-  })
+      }
+    });
 };
 
 function viewAllDepartments() {
-  db.query("select * from department;", function (err, data) {});
+  db.query("select * from department;", (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    console.table(result);
+    questions();
+  });
+}
+function viewAllRoles() {
+  db.query(
+    `SELECT role.id, role.title, role.salary, department.name AS department
+    FROM role
+    LEFT JOIN department ON role.department_id = department.id
+    `,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log(result);
+      questions();
+    }
+  );
+}
+function viewAllEmployees() {
+  db.query(
+    `SELECT
+    employee.id,
+    employee.first_name,
+    employee.last_name,
+    role.title AS job.title,
+    department.name AS department,
+    role.salary,
+    manager.last_name AS manager
+    FROM employee
+    LEFT JOIN role ON employee.role_id = role.id
+    LEFT JOIN department ON role.department_id = department.id
+    LEFT JOIN employee AS manager ON employee.manager_id = manager.id
+    `,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log(result);
+      questions();
+    }
+  );
+}
+function addDepartment() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "newDepartment",
+        message: "What is the name of the new department?",
+      },
+    ])
+    .then((result) => {
+      const { newDepartment } = result;
+    });
+  db.query(`INSERT INTO department (name)`, [newDepartment], (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(result);
+    questions();
+  });
+}
+function addRoles() {
+  db.query(`DELETE FROM course_names WHERE id = ?`, 3, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(result);
+    questions();
+  });
+}
+function addEmployee() {
+  db.query(`DELETE FROM course_names WHERE id = ?`, 3, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(result);
+    questions();
+  });
+}
+
+function updateEmployeeRole() {
+  db.query(`DELETE FROM course_names WHERE id = ?`, 3, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(result);
+    questions();
+  });
 }
